d7450526b2b4218d357c3dea14431895f5af11b9
JP <jaiminbariyastudent@gmail.com>
2024-04-21 11:23:54 +0530

    Number guessing game done, learned - how to change the value og gloabl scope inside a fucntion using gloabl keyword and using return (var -1) and capture return value inside a variable in program

---
+++ 11 - Blackjack/docs.py
@@ -0,0 +1,8 @@
+from art import logo
+import random
+import os
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+PLAYERSCARDS = []
+DELEARSCARDS = []
+DELEARSHWOCARDS = []
---
+++ 12 - Number Guessing Game/main.py
@@ -0,0 +1,64 @@
+from random import randint
+from art import logo
+EASY_LEVEL_ATTEMPTS = 10
+HARD_LEVEL_ATTEMPT = 5
+
+
+
+
+
+def check_answer(guess, answer, attempt):
+    
+    if guess < answer:
+        print("Too low")
+        print("Guess again")
+        return attempt - 1
+
+    elif guess > answer:
+        print("Too High")
+        print("Guess again")
+        return attempt - 1
+    
+    else:
+        print(f"You got it!, Your guess {guess} is correct")
+
+
+
+
+def game():
+    print(logo)
+    print("Welcome to the number guessing game")
+    print("I am thinking a number between 1 to 100")
+    level = input("Choose difficulty level easy or hard").lower()
+
+
+    if level == "easy":
+        attempt = 10
+    elif level == "hard":
+        attempt = 5
+    else:
+        print("valid input")
+    
+    answer = randint(1, 100)
+
+
+    guess = 0
+
+    while (guess != answer):
+
+        print(f"You have {attempt} attempts remaining to guess the number.")
+
+        guess = int(input("Guess any number :"))
+
+        attempt = check_answer(guess, answer, attempt)
+
+
+        if attempt == 0:
+            print("You lose this game, best of luck for next time...")
+            return
+        
+
+game()
+
+
+
